7aab220140192c8ca68bc8261931b373968da06c
JungHun98 <j7papa@naver.com>
2023-10-18 14:21:39 +0900

    test: CI.CD 테스트 커밋

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import GlobalStyle from './style/global.tsx';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <GlobalStyle />
